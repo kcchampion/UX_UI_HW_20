0ae617fed918c87188acff56975c8c9ec42212ff
kcchampion <125626172+kcchampion@users.noreply.github.com>
2023-05-08 20:32:08 -0500

    hw

--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-console.log("Your index.js file is loaded correctly!");
-const myButton = document.getElementById('myButton');
-
-myButton.addEventListener('click', () => {
-  // Code to run when the button is clicked
-  console.log('Button clicked!');
-});
